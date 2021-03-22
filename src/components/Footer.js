6c5d0bd30b3ffe9fe6c7649ed33e58b6cc3f350d
Asia <asia.dobrowolska95@gmail.com>
2021-03-22 12:17:58 +0100

    add styles to Header & Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,38 @@
 import React from "react";
 
+import styled from "styled-components";
+
+const FooterBox = styled.footer`
+  padding: 20px 0;
+  text-align: center;
+  color: #292929;
+`;
+
+const FooterText = styled.p`
+  margin: ${(props) => (props.margin ? "0 0 4px" : 0)};
+
+  font-size: 1.4rem;
+
+  @media (min-width: 1024px) {
+    font-size: 1.6rem;
+  }
+`;
+
+const FooterLink = styled.a`
+  color: #292929;
+  text-decoration: none;
+`;
+
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <p>DevelopMeg 2021 &copy; All right reserved</p>
-      </div>
-    </footer>
+    <FooterBox>
+      <FooterText margin>DevelopMeg 2021 &copy; All right reserved</FooterText>
+      <FooterText>
+        photos by <FooterLink href="https://unsplash.com/">Unsplash</FooterLink>{" "}
+        &amp; loader icon by{" "}
+        <FooterLink href="https://www.freepik.com">Freepik</FooterLink>
+      </FooterText>
+    </FooterBox>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,59 @@
 import React, { useContext, useState, useEffect } from "react";
 
+import { DataFetchContext } from "context/DataFetchContext";
+
 import { useHistory, useLocation } from "react-router-dom";
 
-import { DataFetchContext } from "context/DataFetchContext";
+import styled from "styled-components";
+
+import logo from "assets/logo.png";
+import imageBtnPollution from "assets/image-btn-pollution.jpg";
+
+const HeaderBox = styled.header`
+  position: relative;
+`;
+
+const MainTitleLogo = styled.h1`
+  margin: 0;
+`;
+
+const Logo = styled.img`
+  margin: 0 auto;
+  display: block;
+  max-width: 100%;
+`;
+
+const OpenPollutionBtn = styled.button`
+  margin: 10px auto 25px;
+  padding: 15px;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${imageBtnPollution});
+  background-size: cover;
+  background-position: 0 58%;
+  border-radius: 10px;
+
+  @media (min-width: 1024px) {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    right: 30px;
+    transform: translateY(-50%);
+  }
+`;
+
+const OpenPollutionBtnText = styled.span`
+  padding: 6px;
+  background-color: rgba(255, 255, 255, 0.45);
+  text-transform: uppercase;
+  font-weight: 500;
+
+  @media (min-width: 1024px) {
+    font-size: 1.8rem;
+  }
+`;
 
 const Header = () => {
   const history = useHistory();
@@ -26,12 +77,16 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <h1>ready for weather</h1>
+    <HeaderBox>
+      <MainTitleLogo>
+        <Logo src={logo} alt="page logo" />
+      </MainTitleLogo>
       {path === "/weather" ? (
-        <button onClick={handleGetPollution}>air pollution</button>
+        <OpenPollutionBtn onClick={handleGetPollution}>
+          <OpenPollutionBtnText>air pollution</OpenPollutionBtnText>
+        </OpenPollutionBtn>
       ) : null}
-    </header>
+    </HeaderBox>
   );
 };
 
